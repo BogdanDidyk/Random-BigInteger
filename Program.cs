81b3c34b5393d98f595c16567f79c9d053b6fe16
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-03-22 21:15:33 +0200

    Added method NextBigInteger() that generates random BigInteger

--- Program.cs
+++ Program.cs
@@ -13,6 +13,23 @@ namespace RandomBigInteger
         {
 
         }
+
+        // Generates BigInteger from 0 to 2^bitLength
+        public BigInteger NextBigInteger(int bitLength)
+        {
+            if (bitLength < 1) return BigInteger.Zero;
+
+            int bytes = bitLength / 8;
+            int bits = bitLength % 8;
+
+            byte[] bs = new byte[bytes + 1];
+            NextBytes(bs);
+
+            byte mask = (byte)(0xFF >> (8 - bits));
+            bs[bs.Length - 1] &= mask;
+
+            return new BigInteger(bs);
+        }
     }
 
     public class Program
